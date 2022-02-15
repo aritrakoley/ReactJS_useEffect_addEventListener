eee0f05ddd44f7c928df4dbefd3b086aeb8b0245
aritrakoley <koley.aritra824@gmail.com>
2022-02-16 01:42:36 +0530

    Add cleanup code to child

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ let appRenderCounter = 0;
 let childRenderCounter = 0;
 
 function Child() {
+  const handleClick = () => console.log("click");
   useEffect(() => {
-    console.log('Child useEffect called');
-    window.addEventListener("click", () => console.log("click"));
+    console.log("Child useEffect called");
+    window.addEventListener("click", handleClick);
+
+    return () => window.removeEventListener("click", handleClick);
   }, []);
-  
+
   childRenderCounter++;
   console.log(`Child component rendered ${childRenderCounter} time(s)`);
-  return <div className="child">Child component rendered {childRenderCounter} time(s)</div>    
+  return <div className="child">Child component rendered {childRenderCounter} time(s)</div>
 }
 
 function App() {
