4dc4e824958b5d7a768f12afe06a3f13ecb8cd02
aritrakoley <koley.aritra824@gmail.com>
2022-02-16 01:36:08 +0530

    Add a child component to handle event listener

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-let renderCounter = 0;
-function App() {
-  const [isOnline, setIsOnline] = useState(false);
+let appRenderCounter = 0;
+let childRenderCounter = 0;
 
+function Child() {
   useEffect(() => {
-    console.log('App useEffect called');
+    console.log('Child useEffect called');
     window.addEventListener("click", () => console.log("click"));
   }, []);
   
-  renderCounter++;
-  console.log(`App component rendered ${renderCounter} time(s)`);
+  childRenderCounter++;
+  console.log(`Child component rendered ${childRenderCounter} time(s)`);
+  return <div className="child">Child component rendered {childRenderCounter} time(s)</div>    
+}
+
+function App() {
+  const [isOnline, setIsOnline] = useState(false);
+  appRenderCounter++;
+  console.log(`App component rendered ${appRenderCounter} time(s)`);
   return (
     <div className="app">
-      <p>App component rendered {renderCounter} time(s)</p>
+      <p>App component rendered {appRenderCounter} time(s)</p>
       <button onClick={() => setIsOnline(!isOnline)}>Toggle Status</button>
       <div className={`status ${isOnline ? "on" : "off"}`}></div>
+      <Child />
     </div>
   );
 }
